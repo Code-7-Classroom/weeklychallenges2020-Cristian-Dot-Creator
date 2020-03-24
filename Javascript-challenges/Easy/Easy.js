9217c25a4d7291ed964c7275aa5d12baf8612a90
Cristian Marquez <cristianmmaquez@gmail.com>
2020-03-23 23:25:46 -0400

    added all files'

---
+++ Javascript-challenges/Easy/Easy.js
@@ -0,0 +1,4 @@
+var nameOne = 'John';
+var nameTwo = 'Thomas';
+var nameLength = (nameTwo.length - nameOne.length );
+console.log('The name ' + nameTwo + ' is longer than' + ' ' + nameOne + ' by' +' ' + nameLength + ' characters')
