d0f5c69bf2908863a3ca0d217f2b66dd3dcefd8a
Cristian-Dot-Creator <cristianmmaquez@gmail.com>
2020-03-30 11:14:19 -0400

    ()

--- checkpoint/arrayCheckpoint.js
+++ checkpoint/arrayCheckpoint.js
@@ -2,5 +2,5 @@ var drinks = ['water','coffee','tea'];
 var foods = ['apple','sandwich','chips'];
 
 var meals = [drinks,foods]
-var flipDaArray = meals.reverse
+var flipDaArray = meals.reverse()
 console.log(flipDaArray);
